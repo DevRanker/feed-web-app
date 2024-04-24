98c103e733c2ef4c1fe29722bf8862f05e4de05e
Aditya Agarwal <adityag2511@gmail.com>
2024-04-23 17:34:48 -0700

    [FeedComponent][Simplified] Create Stars SVG into a separate component

--- components/feed.tsx
+++ components/feed.tsx
@@ -1,6 +1,19 @@
 import {Accordion, AccordionItem} from "@nextui-org/react";
 import { Link } from "@nextui-org/link";
 
+
+export function Star() {
+	return (
+		<svg aria-hidden="true"
+			viewBox="0 0 16 16"
+			stroke="currentColor"
+			className="item-center w-4 h-4 mr-1 text-gray-300"
+			>
+				<path d="M8 .25a.75.75 0 0 1 .673.418l1.882 3.815 4.21.612a.75.75 0 0 1 .416 1.279l-3.046 2.97.719 4.192a.751.751 0 0 1-1.088.791L8 12.347l-3.766 1.98a.75.75 0 0 1-1.088-.79l.72-4.194L.818 6.374a.75.75 0 0 1 .416-1.28l4.21-.611L7.327.668A.75.75 0 0 1 8 .25Zm0 2.445L6.615 5.5a.75.75 0 0 1-.564.41l-3.097.45 2.24 2.184a.75.75 0 0 1 .216.664l-.528 3.084 2.769-1.456a.75.75 0 0 1 .698 0l2.77 1.456-.53-3.084a.75.75 0 0 1 .216-.664l2.24-2.183-3.096-.45a.75.75 0 0 1-.564-.41L8 2.694Z"></path>
+		</svg>
+	);
+}
+
 export function Feed() {
 	const item = {
 		repo_id: 772699441,
@@ -25,14 +38,7 @@ export function Feed() {
 					}
 					subtitle={
 						<span class="flex">
-							Up by&nbsp;<strong>{item.new_stars}&nbsp;</strong>
-							<svg aria-hidden="true"
-								viewBox="0 0 16 16"
-								stroke="currentColor"
-								className="item-center w-4 h-4 mr-1 text-gray-300"
-								>
-									<path d="M8 .25a.75.75 0 0 1 .673.418l1.882 3.815 4.21.612a.75.75 0 0 1 .416 1.279l-3.046 2.97.719 4.192a.751.751 0 0 1-1.088.791L8 12.347l-3.766 1.98a.75.75 0 0 1-1.088-.79l.72-4.194L.818 6.374a.75.75 0 0 1 .416-1.28l4.21-.611L7.327.668A.75.75 0 0 1 8 .25Zm0 2.445L6.615 5.5a.75.75 0 0 1-.564.41l-3.097.45 2.24 2.184a.75.75 0 0 1 .216.664l-.528 3.084 2.769-1.456a.75.75 0 0 1 .698 0l2.77 1.456-.53-3.084a.75.75 0 0 1 .216-.664l2.24-2.183-3.096-.45a.75.75 0 0 1-.564-.41L8 2.694Z"></path>
-							</svg>
+							Up by&nbsp;<strong>{item.new_stars}&nbsp;</strong><Star/>
 						</span>
 					}
 					>
