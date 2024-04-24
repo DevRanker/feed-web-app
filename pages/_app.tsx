2dafa41d1b6d8686de4fdfdbee57b66a2125c627
Aditya Agarwal <adityag2511@gmail.com>
2024-04-24 04:56:01 -0700

    [Analytics][Add][PostHog]

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,16 +6,31 @@ import { fontSans, fontMono } from "@/config/fonts";
 import {useRouter} from 'next/router';
 import "@/styles/globals.css";
 
-export default function App({ Component, pageProps }: AppProps) {
+import posthog from "posthog-js"
+import { PostHogProvider } from 'posthog-js/react'
+
+// From PostHog
+if (typeof window !== 'undefined') { // checks that we are client-side
+  posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
+    api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST || 'https://us.i.posthog.com',
+    loaded: (posthog) => {
+      if (process.env.NODE_ENV === 'development') posthog.debug() // debug mode in development
+    },
+  })
+}
+
+export default function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   const router = useRouter();
 
 	return (
-		<NextUIProvider navigate={router.push}>
-			<NextThemesProvider defaultTheme = 'dark'>
-				{/* @ts-expect-error Server Component */}
-				<Component {...pageProps} />
-			</NextThemesProvider>
-		</NextUIProvider>
+		<PostHogProvider client={posthog}>
+			<NextUIProvider navigate={router.push}>
+				<NextThemesProvider defaultTheme = 'dark'>
+					{/* @ts-expect-error Server Component */}
+					<Component {...pageProps} />
+				</NextThemesProvider>
+			</NextUIProvider>
+		</PostHogProvider>
 	);
 }
 
