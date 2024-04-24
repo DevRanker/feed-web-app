e14d59ed84337df305d60ea421f5889f2ebe4b1c
Aditya Agarwal <adityag2511@gmail.com>
2024-04-23 18:29:53 -0700

    [API][trending_repositories] Create an API endpoint to return the list of trending repositories and the last time they were updated with dummy data

---
+++ pages/api/trending_repositories.ts
@@ -0,0 +1,19 @@
+const item = {
+  repo_id: 772699441,
+  repo_full_name: "meta-llama/llama3",
+  repo_link: "https://github.com/meta-llama/llama3",
+  repo_github_url: "https://api.github.com/repos/meta-llama/llama3",
+  new_stars: "59",
+  rank: "1"
+}
+
+export default function handler(req,res) {
+  const trending_repositories = {
+    update_at: 1713921819,
+    repo_list: [
+      item,
+      item
+    ]
+  };
+  res.status(200).json(trending_repositories);
+}
