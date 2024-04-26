263f612d16abeb626b706189caafd2aacb3da453
Aditya Agarwal <adityag2511@gmail.com>
2024-04-25 20:36:36 -0700

    [FeedComponent][New][ReadMeDisplay] Created a 3 column grid to show repos in one and the README content in remaining two using an iframe. (Tailwind reqiures special handling to render HTML (check typography)

--- components/feed.tsx
+++ components/feed.tsx
@@ -1,7 +1,7 @@
 import {Accordion, AccordionItem} from "@nextui-org/react";
 import { Link } from "@nextui-org/link";
 import { useState, useEffect } from 'react';
-
+import {marked} from 'marked';
 
 export function Star() {
 	return (
@@ -19,8 +19,37 @@ export function Feed() {
 	const [trendingRepos, setTrendingRepos] = useState([]);
 	const [updateAt, setUpdatedAt] = useState(0);
 	const trendingReposUrl = process.env.NEXT_PUBLIC_TRENDING_REPOSITORIES_URL!;
+	const [selectedKeys, setSelectedKeys] = useState(new Set([]));
+	const [selectedRepoIndex, setSelectedRepoIndex] = useState();
+	const [markdownReady, setMarkdownReady] = useState(false);
+	const [markdownHTML, setMarkdownHTML] = useState('<p>a</p>');
+
+	const getRepoReadme = async (repo_item) => {
+		const readmeUrl = `https://raw.githubusercontent.com/${repo_item['repo_full_name']}/main/README.md`;
+		fetch(readmeUrl)
+			.then(res => res.text())
+			.then((text) => {
+				setMarkdownHTML(marked.parse(text));
+				setMarkdownReady(true);
+			})
+	};
+
+	useEffect(() => {
+		if (selectedRepoIndex) {
+			setMarkdownReady(false);
+			getRepoReadme(trendingRepos[selectedRepoIndex]);
+		}
+	}, [selectedRepoIndex])
+
+	useEffect(() => {
+		if (selectedKeys.size > 0){
+			const [first] = selectedKeys;
+			setSelectedRepoIndex(first);						
+		}
+	}, [selectedKeys])
 
 	useEffect(()=>{
+
 		fetch(trendingReposUrl)
 			.then((res) => {
 				return res.json();
@@ -28,31 +57,51 @@ export function Feed() {
 			.then((data) => {
 				setTrendingRepos(data.repo_list);
 				setUpdatedAt(data.update_at);
+				setSelectedKeys(new Set(["0"]));
+
 			});
 	},[trendingReposUrl]);
 
 	return (
-		<div className="inline-block max-w-2xl w-screen">
-			<Accordion  variant="bordered" hideIndicator>
-				{trendingRepos.map((repo_item, i) => (
-					<AccordionItem
-						key={repo_item['rank']}
-						aria-label={repo_item['repo_full_name']}
-						startContent={repo_item['rank']}
-						title={
-							<Link isExternal href={repo_item['repo_link']}>
-								{repo_item['repo_full_name']}
-							</Link>
+		<div className="inline-block">
+			<div className="grid grid-cols-3 gap-3">
+				<div className="max-h-[80vh] overflow-auto">
+					<Accordion
+						variant="bordered"
+						hideIndicator
+						selectedKeys={selectedKeys}
+						onSelectionChange={setSelectedKeys}
+					>
+						{trendingRepos.map((repo_item, i) => (
+							<AccordionItem
+								key={i}
+								aria-label={repo_item['repo_full_name']}
+								startContent={repo_item['rank']}
+								title={
+									<Link isExternal href={repo_item['repo_link']}>
+										{repo_item['repo_full_name']}
+									</Link>
+								}
+								subtitle={
+									<span className="flex">
+										Up by&nbsp;<strong>{repo_item['new_stars']}&nbsp;</strong><Star/>
+									</span>
+								}
+								>
+							</AccordionItem>
+						))}
+					</Accordion>
+				</div>
+				<div className="col-span-2">
+					{selectedRepoIndex &&
+						<Link isExternal
+							href={trendingRepos[selectedRepoIndex]['repo_link']}
+							className="mb-4 font-extrabold tracking-tight md:text-4xl dark:text-white">{trendingRepos[selectedRepoIndex]['repo_full_name']}
+						</Link>
 						}
-						subtitle={
-							<span className="flex">
-								Up by&nbsp;<strong>{repo_item['new_stars']}&nbsp;</strong><Star/>
-							</span>
-						}
-						>
-					</AccordionItem>
-				))}
-			</Accordion>
+					{markdownReady && <iframe className=" w-full  h-[80vh] overflow-auto border-solid border-2" srcdoc={markdownHTML}></iframe>}
+				</div>
+			</div>
 		</div>
 	);
 }
