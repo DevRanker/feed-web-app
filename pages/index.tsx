0a9306459f17566275400a0efc8c9d89412a5b79
Aditya Agarwal <adityag2511@gmail.com>
2024-04-25 20:38:41 -0700

    [index][heading] Changed the heading to show within one line and have a more meaingful message

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,15 +6,12 @@ export default function IndexPage() {
 	return (
 		<DefaultLayout>
 			<section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
-				<div className="inline-block max-w-lg text-center justify-center">
-					<h1 className={title()}>Stay&nbsp;</h1>
-					<h1 className={title({ color: "violet" })}>Relevant&nbsp;</h1>
-					<br/>
-					<h1 className={title()}>Know&nbsp;</h1>
-					<h1 className={title({ color: "violet" })}>Dev Trends&nbsp;</h1>
+				<div className="inline-block max-w-2xl text-center justify-center">
+					<h1 className={title()}>Trending &nbsp;</h1>
+					<h1 className={title({ color: "violet" })}>GitHub Repositories&nbsp;</h1>
 					<br />
 					<h4 className={subtitle({ class: "mt-4" })}>
-						(Personalization & More Coming soon)
+						(Personalization Coming soon...)
 					</h4>
 				</div>
 				<Feed/>
