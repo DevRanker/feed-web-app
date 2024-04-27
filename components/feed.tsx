85c468a2a380d58f87f498a92f760113fe5d9c10
Aditya Agarwal <adityag2511@gmail.com>
2024-04-26 20:04:45 -0700

    [StarComponent][NewFile] Moved  component to a separate file under @/components/svgs/star.tsx

--- components/feed.tsx
+++ components/feed.tsx
@@ -4,18 +4,7 @@ import {Accordion, AccordionItem} from "@nextui-org/react";
 import { Link } from "@nextui-org/link";
 import { useState, useEffect } from 'react';
 import {marked} from 'marked';
-
-export function Star() {
-	return (
-		<svg aria-hidden="true"
-			viewBox="0 0 16 16"
-			stroke="currentColor"
-			className="item-center w-4 h-4 mr-1 text-gray-300"
-			>
-				<path d="M8 .25a.75.75 0 0 1 .673.418l1.882 3.815 4.21.612a.75.75 0 0 1 .416 1.279l-3.046 2.97.719 4.192a.751.751 0 0 1-1.088.791L8 12.347l-3.766 1.98a.75.75 0 0 1-1.088-.79l.72-4.194L.818 6.374a.75.75 0 0 1 .416-1.28l4.21-.611L7.327.668A.75.75 0 0 1 8 .25Zm0 2.445L6.615 5.5a.75.75 0 0 1-.564.41l-3.097.45 2.24 2.184a.75.75 0 0 1 .216.664l-.528 3.084 2.769-1.456a.75.75 0 0 1 .698 0l2.77 1.456-.53-3.084a.75.75 0 0 1 .216-.664l2.24-2.183-3.096-.45a.75.75 0 0 1-.564-.41L8 2.694Z"></path>
-		</svg>
-	);
-}
+import {Star} from '@/components/svgs/star';
 
 export function Feed() {
 	const [trendingRepos, setTrendingRepos] = useState([]);
@@ -29,7 +18,6 @@ export function Feed() {
 	    trigger: "data-[hover=true]:bg-default-100 rounded-lg flex items-center"
 	};
 
-
 	const getRepoReadme = async (repo_item: String) => {
 		const readmeUrl = `https://raw.githubusercontent.com/${repo_item['repo_full_name']}/${repo_item['repository_details']['default_branch']}/README.md`;
 
@@ -39,7 +27,7 @@ export function Feed() {
 				{/* @ts-expect-error */}
 				setMarkdownHTML(marked.parse(text));
 				setMarkdownReady(true);
-			})
+			});
 	};
 
 	useEffect(() => {
